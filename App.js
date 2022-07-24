234a9bf14e8dbd0fbc411fc8778f17ec49853ef3
wardedcoffee <marcozzymandias@gmail.com>
2022-07-24 14:24:34 -0300

    criacao do arquivo Cesta.js - componente de funcao

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
